26a5f929859381b9b4a063c532dde05e75e24ead
J <j@localhost.localdomain>
2019-07-01 07:16:54 +0000

    content size fixed

--- obaka/settings.py
+++ obaka/settings.py
@@ -117,13 +117,12 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'obaka/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
