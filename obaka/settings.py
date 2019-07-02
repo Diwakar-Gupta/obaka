94c4de15cb319f7d0ce929ac46820b950f88180e
Diwakar-Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2019-07-02 01:57:31 +0000

    Update settings.py

--- obaka/settings.py
+++ obaka/settings.py
@@ -122,7 +122,7 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'LMS/static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
